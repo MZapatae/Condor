2e8ede4ec9a7f9b2510cbbe19e570153ffd2d7e5
Miguel A. Zapata <miguel.zapatae@gmail.com>
2015-09-27 13:28:48 -0300

    Initial Commit

---
+++ app/src/main/java/cl/mzapatae/condor/LaunchApp.java
@@ -0,0 +1,15 @@
+package cl.mzapatae.condor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LaunchApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_app);
+    }
+}
